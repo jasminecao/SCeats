c114cd4ecf4f612c8ce68ed67803dd178fa8bd80
jasminecao <jasminecao03@gmail.com>
2020-03-22 21:52:26 -0400

    added map

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './Header.js';
 import MapContainer from './MapContainer.js';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 function App() {
   return (
     <>
-      <body>
       <Header/>
       <MapContainer/>
-      </body>
     </>
   );
 }
