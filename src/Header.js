aa29f3293aa6f9b4901189ed4406916e60ec99b6
jasminecao <jasminecao03@gmail.com>
2020-03-22 21:06:44 -0400

    header

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <h1>SCeats</h1>
+        <span>A map of restaurants in State College, PA. </span>
+      </div>
+    </>
+  )
+}
+
+export default Header;
